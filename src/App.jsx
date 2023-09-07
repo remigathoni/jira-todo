0e4d9c570936fa1fbc53ae9b442932e655735748
Mary Gathoni <maryg.kahiga@gmail.com>
2023-09-07 03:36:29 +0300

    Display issues in browser

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from "react";
+
+function App() {
+  const SERVER_BASE_URL=import.meta.env.VITE_SERVER_BASE_URL
+  // eslint-disable-next-line no-unused-vars
+  const [data, setData] = useState([]);
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch(`${SERVER_BASE_URL}/issues/all`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setData(data.issues);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }, [SERVER_BASE_URL]);
+
+
+    useEffect(() => {
+    // Fetch data when the component mounts
+    fetchData();
+  }, [fetchData]);
+  return (
+    <div>
+      <h1 style={{textAlign: "center", fontSize:"1.6rem", marginTop:"1rem"}}>What&apos;s on my list today?</h1>
+      <section style={{display: "flex", flexDirection:"column", justifyContent:"center", alignItems:"center", marginTop:"2rem"}}>
+        {data && data.map(issue => {
+        return <div style={{display: "flex", minWidth:"350px", justifyContent:"space-between", padding:"1rem", backgroundColor:"#eee", marginBottom:"1rem"}} key={issue.id}>
+          <div>{issue.summary}</div>
+          <div style={{padding:"4px", border:"1px solid #000", borderRadius:"5px"}}>{issue.status}</div>
+        </div>
+      })}
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LinkButton.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+
+const LinkButton = ({ url, text }) => {
+  return (
+    <a href={url} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      {text}
+    </a>
+  );
+};
+
+export default LinkButton;
