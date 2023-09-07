d7e0d2151390816011480d9eb7ba4465c86e7218
Mary Gathoni <maryg.kahiga@gmail.com>
2023-09-07 18:49:21 +0300

    Display deadlines

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,30 @@ function App() {
       console.error('Error fetching data:', error);
     }
   }, [SERVER_BASE_URL]);
-
+  console.log(data)
 
     useEffect(() => {
     // Fetch data when the component mounts
     fetchData();
   }, [fetchData]);
-  return (
-    <div>
-      <h1 style={{textAlign: "center", fontSize:"1.6rem", marginTop:"1rem"}}>What&apos;s on my list today?</h1>
-      <section style={{display: "flex", flexDirection:"column", justifyContent:"center", alignItems:"center", marginTop:"2rem"}}>
-        {data && data.map(issue => {
-        return <div style={{display: "flex", minWidth:"350px", justifyContent:"space-between", padding:"1rem", backgroundColor:"#eee", marginBottom:"1rem"}} key={issue.id}>
+return (
+  <div>
+    <h1 style={{textAlign: "center", fontSize:"1.6rem", marginTop:"1rem"}}>What&apos;s on my list today?</h1>
+    <section style={{display: "flex", flexDirection:"column", justifyContent:"center", alignItems:"center", marginTop:"2rem"}}>
+      {data && data.map(issue => {
+      return <div style={{display: "flex", minWidth:"350px", justifyContent:"space-between", padding:"1rem", backgroundColor:"#eee", marginBottom:"1rem"}} key={issue.id}>
+        <div style={{}}>
           <div>{issue.summary}</div>
+          <small style={{color: "gray"}}>{issue.deadline}</small>
+        </div>
+        <div>
           <div style={{padding:"4px", border:"1px solid #000", borderRadius:"5px"}}>{issue.status}</div>
         </div>
-      })}
-      </section>
-    </div>
-  );
+      </div>
+    })}
+    </section>
+  </div>
+);
 }
 
 export default App;
